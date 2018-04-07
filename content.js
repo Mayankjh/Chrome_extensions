965f659900984e6cd959efeac855fa8d1c155e86
Mayank Jha <mayankjha7722@gmail.com>
2018-04-07 21:12:28 +0530

    done!!

--- content.js
+++ content.js
@@ -1,8 +1,25 @@
-console.log("my cool chrome extension readt to boost!!");
-(function() {
-  console.log("bookmarklet starting");
-  let paragraphs = document.getElementsByTagName('p');
-  for(let i= 0; i<paragraphs.length; i++){
-    paragraphs[i].style['background-color'] = '#C036F3';
+console.log("my cool chrome extension ready to boost!!");
+
+// Content scripts can manipulate the DOM
+// Here I'm changing the background for all the paragraphs
+var elts = document.getElementsByTagName('p');
+for (var i = 0; i < elts.length; i++) {
+  elts[i].style['background-color'] = '#CCC';
+}
+
+// Listen for messages
+chrome.runtime.onMessage.addListener(receiver);
+
+// Handle the message
+function receiver(request, sender, sendResponse) {
+  // Now if the message matches "browser action"
+  if (request.message === "browser action") {
+    // Change the background color again
+    var elts = document.getElementsByTagName('p');
+    for (var i = 0; i < elts.length; i++) {
+      elts[i].style['background-color'] = '#F0C';
+    }
+    // Send a message back!
+    chrome.runtime.sendMessage({ "message": "thank you" });
   }
-})();
+}
